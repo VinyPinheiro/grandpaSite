a32f1f6e3faf3b42231ef735e5f9eefd0b41dab8
VinyPinheiro <viny-pinheiro@hotmail.com>
2016-07-29 13:12:14 -0300

    issue #25 Create a Category model class

---
+++ site/class/model/categoryModel.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * file: CategoryDAO.php
+ * class to persist data in database tfrom questionModel
+ */
+
+require_once(realpath('.') . '/class/model/videoModel.php');
+
+class CategoryModel
+{
+	/* Class Attributes */
+	
+	private $name; // String Not null with the name of the category 
+	private $identifier; // Integer with the code of category or not null if the category is a new
+	private $owner; // UserModel object with the creator of this category
+	private $son_category; // Array with Categories daughters of this category, value is null if attribute videos have values
+	private $videos; // Array with Videos belonging to this category, value is null if attribute son_category have values
+
+	/* Method */
+	
+	/**
+	 * @param name String Not null with the name of the category 
+	 * @param owner UserModel object with the creator of this category
+	 * @param videos Array with Videos belonging to this category, value is null if attribute son_category have values
+	 * @param son_category Array with Categories daughters of this category, value is null if attribute videos have values
+	 * @param identifier Integer with the code of category or not null if the category is a new
+	 */
+	public function __construct($name, $owner, $videos = NULL, $son_category = NULL, $identifier = NULL)
+	{
+		
+	}
+
+
+	/* Getters and Setters */
+	
+
+}
+
+?>
