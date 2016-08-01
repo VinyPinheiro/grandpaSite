a4ddc5393b938714c27ef7b9210f593d697dea0d
VinyPinheiro <viny-pinheiro@hotmail.com>
2016-07-31 23:01:47 -0300

    issue #27 fix failed tests

--- site/tests/database.categoryDaoTest.php
+++ site/tests/database.categoryDaoTest.php
@@ -12,16 +12,15 @@ class CategoryDaoTest extends PHPUnit_Framework_TestCase
 	private static $category;
 	private static $questions;
 	
-	const DEFAULT_IDENTIFIER = 12678;
 	
 	protected function setUp()
 	{
 		$image_default_path = realpath('.') . '/user_image/default.png';
         self::$user = new UserModel("Vinicius Pinheiro","viny-pinheiro@hotmail.com","123456789","1995-02-14","MAN","ADMINISTRATOR");
 
-		$this->category = new CategoryModel("Nome",self::$user,NULL,NULL);
+		self::$category = new CategoryModel("Nome",self::$user,NULL,NULL);
 		
-		$this->category_dao = new CategoryDAO(Globals::HOST, Globals::USER, Globals::PASSWORD, Globals::DATABASE,$this->category);
+		self::$category_dao = new CategoryDAO(Globals::HOST, Globals::USER, Globals::PASSWORD, Globals::DATABASE,self::$category);
 
 	}
 	
@@ -33,7 +32,7 @@ class CategoryDaoTest extends PHPUnit_Framework_TestCase
 	
 	public function testCreateCategoryDao()
 	{
-		assert($this->category_dao->getCategoryModel() == $this->category);
+		assert(self::$category_dao->getCategoryModel() == self::$category);
 	}
 		
 	/**
